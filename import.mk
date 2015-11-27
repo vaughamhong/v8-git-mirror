964e4f05fb68a0f2fbeeedbc24380aee0b3e0107
Vaugham Hong <vaughamhong@gmail.com>
2015-11-26 21:42:55 -0800

    added import.mk for android build

---
+++ import.mk
@@ -0,0 +1,17 @@
+################################################################################
+
+# save off the local path
+LOCAL_PATH_TEMP := $(LOCAL_PATH)
+LOCAL_PATH := $(call my-dir)
+
+################################################################################
+
+LOCAL_C_INCLUDES += $(LOCAL_PATH)
+LOCAL_C_INCLUDES += $(LOCAL_PATH)/include
+
+################################################################################
+
+# Restore the local path since we overwrote it when we started
+LOCAL_PATH := $(LOCAL_PATH_TEMP)
+
+################################################################################
